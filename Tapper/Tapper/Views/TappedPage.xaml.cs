8f00027ff8901a376edb4584dd83b1b06defcbbb
David Malý <malyce.dvd@gmail.com>
2016-11-16 11:34:25 +0100

    Added game concept
    Needs refactor

--- Tapper/Tapper/Views/TappedPage.xaml.cs
+++ Tapper/Tapper/Views/TappedPage.xaml.cs
@@ -3,36 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 using Xamarin.Forms;
 
 namespace Tapper.Views
 {
-    
+
     public partial class TappedPage : ContentPage
     {
-      
+        Random cords = new Random();
+
+        TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
         public TappedPage()
         {
             InitializeComponent();
-
+            /*
             TappedView.GestureRecognizers.Add(new TapGestureRecognizer
             {
                 Command = new Command(Tap)
             });
+            */
 
-            SetTappedGestureRecognizer();
+            TapGestureRecognizer viewTap = new TapGestureRecognizer();
+            viewTap.Tapped += (s, e) => {
+                _tappedScore--;
+                show();
+                Tap(true);
+            };
+            TappedView.GestureRecognizers.Add(viewTap);
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () => {
+                Tap(false);
+                return true; //not continue
+            });
+
+            //SetTappedGestureRecognizer();
         }
 
-        private void show(EventArgs e)
+        private void show()
         {
-            TappedLabel.Text = e.ToString();
+            TappedLabel.Text = _tappedScore+"";
             
         }
-        private async void Tap()
+        private async void Tap(bool mistake)
         {
         
-            Label labelToAnimate = createTapZone();
+            Label labelToAnimate = createTapZone(mistake);
 
             TappedView.Children.Add(labelToAnimate);
 
@@ -42,7 +58,7 @@ namespace Tapper.Views
             TappedView.Children.Remove(labelToAnimate);
         }
 
-        private Label createTapZone()
+        private Label createTapZone(bool mistake)
         {
             Label label = new Label
             {
@@ -53,6 +69,7 @@ namespace Tapper.Views
                 VerticalTextAlignment = TextAlignment.Center,
                 BackgroundColor = Color.Blue
             };
+            if (mistake) label.BackgroundColor = Color.Red;
             SetCords(label);
             SetClickedMethod(label);
             return label;
@@ -60,29 +77,31 @@ namespace Tapper.Views
 
         private void SetCords(View view)
         {
-            AbsoluteLayout.SetLayoutBounds(view, new Rectangle(.5, .5, .2, .2));
-            AbsoluteLayout.SetLayoutFlags(view, AbsoluteLayoutFlags.All);
+            
+            AbsoluteLayout.SetLayoutBounds(view, new Rectangle(cords.NextDouble(), cords.NextDouble(), 45, 45));
+            AbsoluteLayout.SetLayoutFlags(view, AbsoluteLayoutFlags.PositionProportional);
         }
 
         private void SetClickedMethod(View view)
         {
-            var tapGestureRecognizer = new TapGestureRecognizer();
-            tapGestureRecognizer.Tapped += (s, e) => {
-                TappedView1(view); 
-            };
+            SetGestureRecognizer(view);
             view.GestureRecognizers.Add(tapGestureRecognizer);
         }
         int _tappedScore = 0;
         private void TappedView1(View view)
         {
-            _tappedScore++;
+            if (view.BackgroundColor == Color.Red) _tappedScore--;
+            else _tappedScore++;
             TappedView.Children.Remove(view);
-            TappedLabel.Text = _tappedScore+"";
+            show();
         }
 
-        private SetGestureRecognizer()
+        private void SetGestureRecognizer(View view)
         {
-
+            tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) => {
+                TappedView1(view);
+            };
         }
     }
 }
